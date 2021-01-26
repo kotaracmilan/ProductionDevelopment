9325eee75fd53e2a3d6266fd3bb2bdcda380eaac
MK <kotaracmilan@votek.solutions>
2021-01-26 17:01:49 +0100

    init

---
+++ ProductionDevelopment/src/test/java/com/kota/pd/ProductionDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kota.pd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
